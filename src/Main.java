f6e6b881b2aa168fc06823d05ec5cb2d8bc41b0d
Тацухиро Сато <taczuhiro@mail.ru>
2022-08-07 15:11:05 +0300

    forkjoin

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,7 +10,12 @@ public class Main {
         String folderPath = "E:\\Program files";
         File file = new File(folderPath);
 
-        System.out.println(getFolderSize(file));
+        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+        System.out.println(size);
+
+//        System.out.println(getFolderSize(file));
     }
 
     public static long getFolderSize(File folder) {
